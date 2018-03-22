1a444393aaa68293d9bc4f16108e4df5ffca4ba1
Yuki Nakamuraya <fourdim53@gmail.com>
2018-03-22 21:27:24 +0900

    refactored followed by rubocop

--- config/application.rb
+++ config/application.rb
@@ -6,13 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-#configlation follows rails-style-guide
+# configlation follows rails-style-guide
 platform = RUBY_PLATFORM.match(/(linux|darwin)/)[0].to_sym
 Bundler.require(platform)
 
 module ChatSpace
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/* take precedence
+    # over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.generators do |g|
