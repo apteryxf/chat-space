ba70f81cb0d9bf0bd594f01c5888aed6aa42ebcf
Yuki Nakamuraya <fourdim53@gmail.com>
2018-03-27 14:00:17 +0900

    set routing, add action, create view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,21 @@
 class GroupsController < ApplicationController
+  def index
+    #code
+  end
+
+  def new
+    #code
+  end
+
+  def create
+    #code
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20180326115231) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
