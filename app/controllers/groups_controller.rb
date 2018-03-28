a74fd13760bf8c7bd0f458ed91dfc22b59ac45a2
Yuki Nakamuraya <fourdim53@gmail.com>
2018-03-28 13:58:29 +0900

    グループ作成機能完成、作成エラーメッセージを表示、ルートパスのルーティングを変更

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,20 @@
 class GroupsController < ApplicationController
   def index
-    #code
+    @groups = current_user.groups
   end
 
   def new
     @group = Group.new
+    @group.users << current_user
   end
 
   def create
-    #code
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,4 +25,8 @@ class GroupsController < ApplicationController
     #code
   end
 
+  private
+  def group_params
+    params.require(:group).permit(:name, {:user_ids => []})
+  end
 end
