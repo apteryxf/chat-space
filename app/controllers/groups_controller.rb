6a8162104070135d57e547b5627a8602078b8953
Yuki Nakamuraya <fourdim53@gmail.com>
2018-03-28 16:16:50 +0900

    complete group create and edit function, partialed form part

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def index
     @groups = current_user.groups
   end
@@ -18,15 +20,23 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    #code
+
   end
 
   def update
-    #code
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, {:user_ids => []})
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
