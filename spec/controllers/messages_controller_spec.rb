8f67f7db2aaa6ce8b102396cd8fe436e8ed23021
Yuki Nakamuraya <fourdim53@gmail.com>
2018-04-02 17:13:43 +0900

    set using devise with rspec

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe MessagesController do
+  context "when user logins" do
+    describe "GET #index" do
+
+    end
+  end
+
+  context "when user doesn't login" do
+    describe "" do
+
+    end
+  end
+end
