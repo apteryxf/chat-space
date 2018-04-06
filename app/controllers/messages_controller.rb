d2c68731bc96e955194991c34c9db20f2d4a932a
Yuki Nakamuraya <fourdim53@gmail.com>
2018-04-06 17:04:06 +0900

    separate html or json case

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,11 @@ class MessagesController < ApplicationController
   def create
     @message  = Message.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージを送信しました'
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(@group) }
+        format.json
+      end
+      flash.now[:notice] = 'メッセージを送信しました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください'
