279900a731b7fe20dc11b69e7520b2132943bb76
Yuki Nakamuraya <fourdim53@gmail.com>
2018-04-07 16:33:48 +0900

    receive json data then add one message html block

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,26 @@
 $(function() {
+  function buildHTML(message) {
+    var html = `<div class="message">
+                  <ul class="message__lists">
+                    <li class="message__lists__list" id="user-name">
+                      ${message.name}
+                    </li>
+                    <li class="message__lists__list" id="date">
+                      ${message.created_at}
+                    </li>
+                  </ul>
+                  <p class="message--content">
+                    ${message.body}
+                    <img src="${message.image.url}">
+                  </p>
+                </div>`
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -11,5 +29,16 @@ $(function() {
       processData: false,
       contentType: false
     })
+    .done(function(data) {
+      console.log(data);
+      var html = buildHTML(data);
+      $('.chat__messages').append(html);
+      $('.chat__form--message').val('');
+      $('.hidden').val('');
+      // $('.chat__messages').animate({
+      //   scrollTop: $(document).height()
+      // });
+    })
+    return false;
   })
 });
