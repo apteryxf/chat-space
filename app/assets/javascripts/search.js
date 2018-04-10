bc38f5fe245650d3cedd3bd99ec8b32f44578871
Yuki Nakamuraya <fourdim53@gmail.com>
2018-04-10 21:28:10 +0900

    asynchronous search users when keyup

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,13 @@
 $(function() {
   $(".search__query").on("keyup", function() {
     var input = $(".search__query").val();
- });
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+  });
 });
