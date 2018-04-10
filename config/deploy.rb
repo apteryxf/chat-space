3603d5d9ffb0dc74049dec0f77cad9d9431ec05d
Yuki Nakamuraya <fourdim53@gmail.com>
2018-04-10 19:50:39 +0900

    set deploy automation
    Elastic IP has changed.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,24 @@ lock "~> 3.10.1"
 
 set :application, "chat-space"
 set :repo_url, "git@github.com:apteryxf/chat-space.git"
-set :stage, :production
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['/Users/youki/.ssh/key_pair.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -38,22 +55,3 @@ set :stage, :production
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
-set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1'
-
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/youki/.ssh/key_pair.pem']
-
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-set :keep_releases, 5
-
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
