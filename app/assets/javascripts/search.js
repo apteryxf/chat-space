776303e06ae1bd9c12cbd23a311ece2ad9d09aae
Yuki Nakamuraya <fourdim53@gmail.com>
2018-04-11 13:58:11 +0900

    add search result html to view

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,13 @@
 $(function() {
+
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`
+    return html;
+  }
+
   $(".search__query").on("keyup", function() {
     var input = $(".search__query").val();
 
@@ -9,5 +18,17 @@ $(function() {
       dataType: 'json'
     })
 
+    .done(function(users) {
+      $("#chat-group-user").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          var html = appendUser(user);
+          $("#chat-group-user").append(html);
+        });
+      }
+      else {
+        appendNoUser("一致するユーザーはいません");
+      }
+    })
   });
 });
