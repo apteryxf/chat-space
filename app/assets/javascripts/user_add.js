18e43e7c102a4690b8c46901f3ae76fe09f89b35
Yuki Nakamuraya <fourdim53@gmail.com>
2018-04-12 19:59:43 +0900

    change like adding user element in function

--- app/assets/javascripts/user_add.js
+++ app/assets/javascripts/user_add.js
@@ -1,12 +1,12 @@
 $(function() {
-  
+
   function addUser(id, name) {
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id}'>
                 <input name='group[user_ids][]' type='hidden' value='${id}'>
                 <p class='chat-group-user__name'>${name}</p>
                 <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`
-    return html;
+    user_list.append(html);
   }
 
   var user_list = $("#chat-group-user")
@@ -15,8 +15,7 @@ $(function() {
     $(this).parent().remove();
     var user_id = $(this).attr('data-user-id');
     var user_name = $(this).attr('data-user-name');
-    var html = addUser(user_id, user_name);
-    user_list.append(html);
+    addUser(user_id, user_name);
   });
 
   $(document).off("click", ".user-search-remove");
