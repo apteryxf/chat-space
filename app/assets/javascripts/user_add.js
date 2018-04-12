c81f757c9512e7721f929a22c749c1a4e4dd6768
Yuki Nakamuraya <fourdim53@gmail.com>
2018-04-12 19:32:28 +0900

    fix

--- app/assets/javascripts/user_add.js
+++ app/assets/javascripts/user_add.js
@@ -3,6 +3,7 @@ $(function() {
   function addUser(id, name) {
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id}'>
                 <input name='group[user_ids][]' type='hidden' value='${id}'>
+                <p class='chat-group-user__name'>${name}</p>
                 <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`
     return html;
