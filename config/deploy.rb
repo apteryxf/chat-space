526189052b419596ab2e111e4bf212d160ff990a
Yuki Nakamuraya <fourdim53@gmail.com>
2018-06-12 05:07:24 +0900

    change ec2 insatnce

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/youki/.ssh/key_pair.pem']
+                  keys: ['/Users/youki/.ssh/cht_key_pair.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
